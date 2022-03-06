f97567159601229386687631afd87f7e57edc2e2
tuosm9390 <tuosm123@naver.com>
2022-03-07 03:38:41 +0900

    portfolio-first build

---
+++ src/components/header/header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocial from "./headerSocials";
+
+const header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Kim Sangchan</h1>
+        <h5 className="text-light">Junior FrontEnd Developer</h5>
+        <CTA />
+        <HeaderSocial />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default header;
---
+++ src/components/header/headerSocials.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiDribbble } from "react-icons/fi";
+
+const headerSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/tuosm9390" target="blank">
+        <FaGithub />
+      </a>
+      <a href="https://dribbble.com" target="blank">
+        <FiDribbble />
+      </a>
+    </div>
+  );
+};
+
+export default headerSocials;
